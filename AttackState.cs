d3018d0d86f2cbc57bfaa159a302a77ede1b2ab3
Henrique <abreu-semperfi@hotmail.com>
2020-05-19 18:51:59 +0100

    added rotate to player in AttackState

--- AttackState.cs
+++ AttackState.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 
 public class AttackState : State
 {
+    float rotSpeed = 2.5f;
     public AttackState(GameObject npc, NavMeshAgent agent, Transform player)
         : base(npc, agent, player)
     {
@@ -24,6 +25,10 @@ public class AttackState : State
 
         if (PlayerInRange())
         {
+            Vector3 dir = player.position - npc.transform.position;
+            float angle = Vector3.Angle(dir, npc.transform.forward);
+            dir.y = 0;
+            npc.transform.rotation = Quaternion.Slerp(npc.transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotSpeed);
             npc.GetComponent<Shoot>().Fire();
         }
         else
